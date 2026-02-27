71aa5bf8d844de64af5337a7176ecd6b056b0551
enzol <lonardoni.enzo@gmail.com>
2026-02-27 13:24:26 +0100

    Move Turns section after Services for better UX flow

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,9 @@ function App() {
       <main>
         <section id="home"><Hero /></section>
         <section id="services"><Services /></section>
+        <section id="turns"><Turns /></section>
         <section id="blogs"><Blogs /></section>
         <section id="about"><About /></section>
-        <section id="turns"><Turns /></section>
         <section id="app"><DownloadSection /></section>
         <section id="contact"><Contact /></section>
       </main>
